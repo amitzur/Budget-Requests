28bbc98684f9e7b54b3e61738e8449043ffa17d5
Amit Zur <sendwithchibo@gmail.com>
2012-03-30 15:45:21 +0300

    user field validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120330120201) do
+ActiveRecord::Schema.define(:version => 20120330124336) do
 
   create_table "bakashas", :force => true do |t|
     t.date     "recv_date"
@@ -54,5 +54,7 @@ ActiveRecord::Schema.define(:version => 20120330120201) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
